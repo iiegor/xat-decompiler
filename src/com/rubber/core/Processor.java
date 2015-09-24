d38217932c944d2e57210f3cfe5be8901d071507
iiegor <iegormundo@hotmail.com>
2015-09-25 00:50:29 +0200

    Update Processor.java

--- src/com/rubber/core/Processor.java
+++ src/com/rubber/core/Processor.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.rubber.core;
 
 import com.rubber.Statics;
@@ -22,9 +17,7 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 
 /**
- * Credits to Nasty35 and rabcdasm
- *
- * @author Iegor
+ * @author Iegor, Nasty35
  */
 public class Processor extends Thread {
 
